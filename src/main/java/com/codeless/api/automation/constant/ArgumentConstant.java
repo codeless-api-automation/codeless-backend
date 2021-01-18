ce5da7bdf62949ecdac0a7dbfb50fd33ff2d71c2
pavelklindziuk <pavelklindziuk@gmail.com>
2021-01-18 20:12:49 +0300

    CDLS-02: restore constants

---
+++ src/main/java/com/codeless/api/automation/constant/ArgumentConstant.java
@@ -0,0 +1,11 @@
+package com.codeless.api.automation.constant;
+
+public final class ArgumentConstant {
+
+  public static final String EXECUTION_ID = "executionType=%s";
+  public static final String EXECUTION_TYPE = "executionType=%s";
+  public static final String SCHEDULE_ID = "executionId=%s";
+
+  private ArgumentConstant() {
+  }
+}
