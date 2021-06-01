efdbfcf4cffc216d21df1afca1d17d0fbe8a593d
Oleg Strunevskiy <oleg.strunevskiy@gmail.com>
2021-05-31 18:04:40 -0700

    Remove auto configuration

--- src/main/java/com/codeless/api/automation/configuration/HttpSessionConfiguration.java
+++ src/main/java/com/codeless/api/automation/configuration/HttpSessionConfiguration.java
@@ -3,11 +3,9 @@ package com.codeless.api.automation.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.http.HeaderHttpSessionIdResolver;
 import org.springframework.session.web.http.HttpSessionIdResolver;
 
-@EnableRedisHttpSession
 @Configuration
 public class HttpSessionConfiguration {
 
