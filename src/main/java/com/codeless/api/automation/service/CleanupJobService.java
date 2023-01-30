09b43d16c995398a799aaf90a971a4e8e027152b
Oleg Strunevskiy <oleg.strunevskiy@gmail.com>
2023-01-29 21:01:17 -0800

    Move class outside impl folder

--- src/main/java/com/codeless/api/automation/service/CleanupJobService.java
+++ src/main/java/com/codeless/api/automation/service/CleanupJobService.java
@@ -1,7 +1,8 @@
-package com.codeless.api.automation.service.impl;
+package com.codeless.api.automation.service;
 
 import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -9,9 +10,13 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class CleanupJobService {
 
+  @Value("${codeless.job.delete-expired-executions.interval-in-cron.expiration-time}")
+  private String expirationTime;
+
   @Scheduled(cron = "${codeless.job.delete-expired-executions.interval-in-cron}")
   @SchedulerLock(name = "delete-expired-executions")
   public void deleteExpiredExecutions() {
+    // TODO: delete old executions and their results
   }
 
 }
