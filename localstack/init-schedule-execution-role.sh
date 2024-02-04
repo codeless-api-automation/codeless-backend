86a878109decc671fb1654f905a4aafea67b4864
Oleg Strunevskiy <oleg.strunevskiy@gmail.com>
2024-02-04 06:09:33 -0800

    Delete multi account setup

--- localstack/init-schedule-execution-role.sh
+++
@@ -1,3 +0,0 @@
-echo $(aws iam create-policy --policy-name schedule-execution-role-policy --policy-document file://schedule/policy.json)
-echo $(aws iam create-role --role-name schedule-execution-role --assume-role-policy-document file://schedule/trust-policy.json)
-echo $(aws iam attach-role-policy --policy-arn arn:aws:iam:*:*:policy/schedule-execution-role-policy --role-name schedule-execution-role)
