00a180c893462b524c19a0a7596205609e914fb7
pavelklindziuk <pavelklindziuk@gmail.com>
2024-02-05 17:49:20 +0300

    CDLS: add maximum size, fix issue with empty beans, add default user

--- localstack/init-dynamodb.sh
+++ localstack/init-dynamodb.sh
@@ -230,19 +230,5 @@ EOF
 )
 
 echo $(aws --endpoint-url ${ENDPOINT} dynamodb create-table --cli-input-json "$TABLE_SCHEDULE")
-
-
-ENABLE_TTL_ON_EXECUTION_TABLE=$(cat <<EOF
-{
-  "TimeToLiveSpecification": {
-    "AttributeName": "ttl",
-    "Enabled": true
-  }
-}
-EOF
-)
-
-echo $(aws dynamodb update-time-to-live --table-name execution --cli-input-json "$ENABLE_TTL_ON_EXECUTION_TABLE")
-
-
+echo $(aws --endpoint-url ${ENDPOINT} dynamodb update-time-to-live --table-name "$TABLE_SCHEDULE" --time-to-live-specification Enabled=true,AttributeName=e_at --region us-east-1)
 echo $(aws --endpoint-url ${ENDPOINT} dynamodb list-tables)
--- src/main/java/com/codeless/api/automation/util/ValidationUtil.java
+++ src/main/java/com/codeless/api/automation/util/ValidationUtil.java
@@ -2,9 +2,11 @@ package com.codeless.api.automation.util;
 
 public final class ValidationUtil {
 
-  private ValidationUtil ()  {}
+  private ValidationUtil() {
+    throw new RuntimeException();
+  }
 
-  public static int validateMaxResults(int maxResults){
+  public static int validateMaxResults(int maxResults) {
     return Math.min(maxResults, 50);
   }
 }
